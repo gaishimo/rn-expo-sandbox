f291ed3e2220b92cd0a7dfeb627cffe8c45c06ed
gaishimo <gaithimo@gmail.com>
2022-10-08 15:11:41 +0900

    Disable config plugin name change

--- plugin/src/withExample.ts
+++ plugin/src/withExample.ts
@@ -3,6 +3,6 @@ import { ConfigPlugin } from "@expo/config-plugins"
 type Props = { name: string }
 
 export const withExample: ConfigPlugin<Props> = (config, props) => {
-  config.name = props.name
+  // config.name = props.name
   return config
 }
